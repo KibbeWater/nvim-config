10986a5670d6b8a08a7a0f0adaaf4f3030a946f2
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2024-07-25 18:37:29 +0200

    feat: init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Setup
+
+```
+git clone https://github.com/KibbeWater/nvim-config.git ~/.config/nvim && nvim
+```
+
+Now run `:MasonInstallAll` to install all the packages
+
---
+++ lua/snow/init.lua
@@ -0,0 +1,19 @@
+print("Hello Snow")
+-- Initialize the plugin system
+vim.cmd [[call plug#begin()]]
+
+-- Add plugins
+vim.cmd [[Plug 'ThePrimeagen/vim-be-good']]
+
+-- End the plugin setup
+vim.cmd [[call plug#end()]]
+
+vim.o.relativenumber = true
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.updatetime = 50
+
+vim.api.nvim_set_keymap(  't'  ,  '<ESC>'  ,  '<C-\\><C-n>'  ,  {noremap = true}  )
+
