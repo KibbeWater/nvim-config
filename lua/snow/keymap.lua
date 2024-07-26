2d1c953bbe8e1a5c36ad0c749c5bfcc9a942b2ed
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2024-07-27 00:41:42 +0200

    feat: keybind to add empty lines above/below

--- lua/snow/keymap.lua
+++ lua/snow/keymap.lua
@@ -1,1 +1,5 @@
-vim.api.nvim_set_keymap(  't'  ,  '<ESC>'  ,  '<C-\\><C-n>'  ,  {noremap = true}  )
+vim.api.nvim_set_keymap("t", "<ESC>", "<C-\\><C-n>", { noremap = true })
+
+-- Insert empty line above or below cursor
+vim.api.nvim_set_keymap("n", "<Space>O", "m`O<Esc>``", { noremap = true })
+vim.api.nvim_set_keymap("n", "<Space>o", "m`o<Esc>``", { noremap = true })
