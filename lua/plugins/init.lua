08c5db6af7c33d3d861de798db4755c2f79ae175
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2024-08-31 01:32:52 +0200

    feat: github copilot plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,50 +1,51 @@
 return {
-  { "ThePrimeagen/vim-be-good", lazy = false },
-  { "tpope/vim-fugitive", lazy = false },
-  {
-    "stevearc/conform.nvim",
-    event = "BufWritePre", -- uncomment for format on save
-    config = function()
-      require "configs.conform"
-    end,
-  },
+    { "ThePrimeagen/vim-be-good", lazy = false },
+    { "tpope/vim-fugitive", lazy = false },
+    { "github/copilot.vim", lazy = false },
+    {
+        "stevearc/conform.nvim",
+        event = "BufWritePre", -- uncomment for format on save
+        config = function()
+            require "configs.conform"
+        end,
+    },
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require("nvchad.configs.lspconfig").defaults()
-      require "configs.lspconfig"
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "lua-language-server",
-        "stylua",
-        "html-lsp",
-        "css-lsp",
-        "gopls",
-        "prettierd",
-        "tailwindcss-language-server",
-        "typescript-language-server",
-        "marksman",
-        "mdx-analyzer",
-        "astro-language-server",
-      },
+    -- These are some examples, uncomment them if you want to see them work!
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require("nvchad.configs.lspconfig").defaults()
+            require "configs.lspconfig"
+        end,
     },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "vim",
-        "lua",
-        "vimdoc",
-        "html",
-        "css",
-      },
+    {
+        "williamboman/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "lua-language-server",
+                "stylua",
+                "html-lsp",
+                "css-lsp",
+                "gopls",
+                "prettierd",
+                "tailwindcss-language-server",
+                "typescript-language-server",
+                "marksman",
+                "mdx-analyzer",
+                "astro-language-server",
+            },
+        },
     },
-  },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                "vim",
+                "lua",
+                "vimdoc",
+                "html",
+                "css",
+            },
+        },
+    },
 }
