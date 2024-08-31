ff134b20324d2df08fb2d87c8c0980b096b357e2
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2024-08-31 02:09:56 +0200

    feat: switched from copilot to avante

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,13 +1,76 @@
 return {
     { "ThePrimeagen/vim-be-good", lazy = false },
     { "tpope/vim-fugitive", lazy = false },
-    { "github/copilot.vim", lazy = false },
     {
         "stevearc/conform.nvim",
         event = "BufWritePre", -- uncomment for format on save
         config = function()
             require "configs.conform"
         end,
+    },
+
+    {
+        "yetone/avante.nvim",
+        event = "VeryLazy",
+        lazy = false,
+        opts = {},
+        keys = {
+            {
+                "<leader>aa",
+                function()
+                    require("avante.api").ask()
+                end,
+                desc = "avante: ask",
+                mode = { "n", "v" },
+            },
+            {
+                "<leader>ar",
+                function()
+                    require("avante.api").refresh()
+                end,
+                desc = "avante: refresh",
+            },
+            {
+                "<leader>ae",
+                function()
+                    require("avante.api").edit()
+                end,
+                desc = "avante: edit",
+                mode = "v",
+            },
+        },
+        dependencies = {
+            "stevearc/dressing.nvim",
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            --- The below dependencies are optional,
+            "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+            {
+                -- support for image pasting
+                "HakonHarnes/img-clip.nvim",
+                event = "VeryLazy",
+                opts = {
+                    -- recommended settings
+                    default = {
+                        embed_image_as_base64 = false,
+                        prompt_for_file_name = false,
+                        drag_and_drop = {
+                            insert_mode = true,
+                        },
+                        -- required for Windows users
+                        use_absolute_path = true,
+                    },
+                },
+            },
+            {
+                -- Make sure to setup it properly if you have lazy=true
+                "MeanderingProgrammer/render-markdown.nvim",
+                opts = {
+                    file_types = { "markdown", "Avante" },
+                },
+                ft = { "markdown", "Avante" },
+            },
+        },
     },
 
     -- These are some examples, uncomment them if you want to see them work!
--- lua/snow/autoupdater.lua
+++ lua/snow/autoupdater.lua
@@ -1,18 +1,25 @@
 local function get_config_path()
-    return vim.fn.stdpath "config"
+    local config_path = vim.fn.stdpath "config"
+    if not config_path then
+        error "Failed to get config path"
+    end
+    return config_path
 end
 
 local function run_git_command(path, command, callback)
+    if not path or not command then
+        error "Invalid arguments for run_git_command"
+    end
     local full_command = string.format("cd %s && %s", path, command)
     vim.fn.jobstart(full_command, {
         on_stdout = function(_, data)
-            if data[1] ~= "" then
+            if data and data[1] and data[1] ~= "" then
                 callback(vim.trim(data[1]))
             end
         end,
         on_stderr = function(_, data)
-            if data[1] ~= "" then
-                print("Error: " .. vim.trim(table.concat(data, "\n")))
+            if data and data[1] and data[1] ~= "" then
+                vim.api.nvim_err_writeln("Error: " .. vim.trim(table.concat(data, "\n")))
             end
         end,
         stdout_buffered = true,
@@ -25,8 +32,16 @@ local function check_git_status()
     local current_hash, remote_hash
 
     run_git_command(config_path, "git rev-parse HEAD", function(cur_hash_result)
+        if not cur_hash_result then
+            vim.api.nvim_err_writeln "Failed to get current hash"
+            return
+        end
         current_hash = cur_hash_result
         run_git_command(config_path, "git ls-remote origin HEAD | cut -f1", function(remote_hash_result)
+            if not remote_hash_result then
+                vim.api.nvim_err_writeln "Failed to get remote hash"
+                return
+            end
             remote_hash = remote_hash_result
             if current_hash ~= remote_hash then
                 print "The config is out of date, run :UpdateConfig to update"
@@ -36,10 +51,14 @@ local function check_git_status()
 end
 
 local function update_config(path)
+    if not path then
+        error "Invalid path for update_config"
+    end
     run_git_command(path, "git pull", function(result)
-        if string.find(result, "Abort") then
-            print "Update was unsuccessful, please discard or stash changes before pulling"
-            return
+        if not result then
+            vim.api.nvim_err_writeln "Update failed: No result from git pull"
+        elseif string.find(result, "Abort") then
+            vim.api.nvim_err_writeln "Update was unsuccessful, please discard or stash changes before pulling"
         else
             print "Update was successful, please restart neovim"
         end
@@ -48,6 +67,10 @@ end
 
 vim.api.nvim_create_user_command("UpdateConfig", function()
     local config_path = get_config_path()
+    if not config_path then
+        vim.api.nvim_err_writeln "Failed to get config path"
+        return
+    end
     update_config(config_path)
 end, {})
 
