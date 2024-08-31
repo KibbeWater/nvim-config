08f1068ea79c947116153da7704ee7a7f7e66778
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2024-09-01 00:01:00 +0200

    chore(keybind): window resizing

--- lua/snow/keymap.lua
+++ lua/snow/keymap.lua
@@ -9,3 +9,9 @@ vim.api.nvim_set_keymap("n", "<Space>ga", ":Git add .<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<Space>gc", ":Git commit<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<Space>gp", ":Git push<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<Space>gs", ":Git<CR>", { noremap = true })
+
+-- Window Resizing
+vim.api.nvim_set_keymap("n", "=", "<cmd>vertical resize +5<CR>", { noremap = true }) -- Make the window bigger vertically
+vim.api.nvim_set_keymap("n", "-", "<cmd>vertical resize -5<CR>", { noremap = true }) -- Make the window smaller vertically
+vim.api.nvim_set_keymap("n", "+", "<cmd>horizontal resize +2<CR>", { noremap = true }) -- Make the window bigger horizontally
+vim.api.nvim_set_keymap("n", "_", "<cmd>horizontal resize -2<CR>", { noremap = true }) -- Make the window smaller horizontally
