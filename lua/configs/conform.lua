700806c0ee16abdd0f17a560f24e8ed1b157a126
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2024-09-03 18:36:49 +0200

    chore: swap from prettierd to prettier

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,31 +1,31 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
+    formatters_by_ft = {
+        lua = { "stylua" },
 
-    typescript = { "prettierd", "prettier", stop_after_first = true },
-    typescriptreact = { "prettierd", "prettier", stop_after_first = true },
-    javascript = { "prettierd", "prettier", stop_after_first = true },
-    javascriptreact = { "prettierd", "prettier", stop_after_first = true },
-    astro = { "prettier", stop_after_first = true },
-  },
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+        typescript = { "prettier", stop_after_first = true },
+        typescriptreact = { "prettier", stop_after_first = true },
+        javascript = { "prettier", stop_after_first = true },
+        javascriptreact = { "prettier", stop_after_first = true },
+        astro = { "prettier", stop_after_first = true },
+    },
+    format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+    },
 
-  formatters = {
-    prettierd = {
-      env = {
-        PRETTIERD_DEFAULT_CONFIG = "~/.config/.prettierrc.js",
-      },
+    formatters = {
+        prettierd = {
+            env = {
+                PRETTIERD_DEFAULT_CONFIG = "~/.config/.prettierrc.js",
+            },
+        },
+        prettier = {
+            env = {
+                PRETTIERD_DEFAULT_CONFIG = "~/.config/.prettierrc.js",
+            },
+        },
     },
-    prettier = {
-      env = {
-        PRETTIERD_DEFAULT_CONFIG = "~/.config/.prettierrc.js",
-      },
-    },
-  },
 }
 
 require("conform").setup(options)
