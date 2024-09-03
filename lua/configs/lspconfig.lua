1936009202a4c824dbae5d116a30966ae55cf4d2
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2024-09-03 18:35:44 +0200

    chore: add XML LSP

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,13 +3,13 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "tsserver", "tailwindcss", "gopls", "marksman", "astro" }
+local servers = { "html", "cssls", "tsserver", "tailwindcss", "gopls", "marksman", "astro", "sourcekit", "lemminx" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-  }
+    lspconfig[lsp].setup {
+        on_attach = on_attach,
+        on_init = on_init,
+        capabilities = capabilities,
+    }
 end
